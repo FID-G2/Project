4c2bc8c4f1d5d72dba7c6cc5ca34b95147c515d8
davbarsal1 <72515663+davbarsal1@users.noreply.github.com>
2023-12-16 20:50:00 +0100

    Arreglo a la hora de definir los datos con ruido

--- Add-noise/add-noise.R
+++ Add-noise/add-noise.R
@@ -1,11 +1,10 @@
 setwd("C:/Users/David/Desktop/WD FID/Project/Add-noise")
-# install.packages("dplyr")
 
 # Cargar el archivo CSV original
 datos <- read.csv("dataset_integrado.csv")
 n <- nrow(datos)
 seed <- 375
-#Porcentaje de filas que serán cambiadas
+# Porcentaje de filas que serán cambiadas
 porcentaje <- 5
 nestudiantes <- porcentaje
 cautonoma <- porcentaje
@@ -14,20 +13,20 @@ universidad <- porcentaje
 set.seed(seed)
 nestudiantesMissingRows <- sample(1:n, round(nestudiantes/100 * n, 0))
 
-set.seed(seed)
+set.seed(seed+seed*seed)
 cautonomaMissingRows <- sample(1:n, round(cautonoma/100 * n, 0))
 
-set.seed(seed)
+set.seed(seed+seed)
 universidadMissingRows <- sample(1:n, round(universidad/100 * n, 0))
 
-datos[nestudiantesMissingRows, "Número.de.estudiantes"] <- NA
-datos[cautonomaMissingRows, "Comunidad.Autónoma"] <- NA
-datos[universidadMissingRows, "Universidad"] <- NA
+datos_ruido <- datos  # Crear una copia de los datos originales
 
+datos_ruido[nestudiantesMissingRows, "Número.de.estudiantes"] <- NA
+datos_ruido[cautonomaMissingRows, "Comunidad.Autónoma"] <- NA
+datos_ruido[universidadMissingRows, "Universidad"] <- NA
 
-cantidad_na_por_columna <- colSums(is.na(datos)) # Comprobar cuántos NA hay por columna
+cantidad_na_por_columna <- colSums(is.na(datos_ruido)) # Comprobar cuántos NA hay por columna
 print(cantidad_na_por_columna)
-
 
 # Guardar el nuevo conjunto de datos con ruido en un archivo CSV
 write.csv(datos_ruido, file = paste0("dataset_ruido_", porcentaje, ".csv"), row.names = FALSE)
